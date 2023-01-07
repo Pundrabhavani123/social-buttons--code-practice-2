ffbbf59981ecd27cc9ac09245804f95cb176cf69
Pundra <pundrabhavani@gmail.com>
2023-01-07 09:35:16 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,23 @@
+const Button = (props) => {
+  //  Write your code here.
+  const { className, buttonText } = props;
+  return <button className={`button ${className}`}>{buttonText}</button>;
+};
+
+const element = (
+  //  Write your code here.
+  <div className="social-buttons-container">
+    <div className="content-container">
+      <h1 className="heading">Social Buttons</h1>
+      <div className="buttons-container">
+        <Button buttonText="Like" className="like-button" />
+
+        <Button buttonText="comment" className="comment-button" />
+
+        <Button buttonText="share" className="share-button" />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
